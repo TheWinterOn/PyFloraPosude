a8e8eaa60ffc72dcc115711d78a88d23aa2ec2ca
TheWinterOn <dzima2@gmail.com>
2023-07-29 15:54:33 +0200

    Added user database and login

--- gui/gui.py
+++ gui/gui.py
@@ -1,69 +1,74 @@
 import tkinter as tk
-
+from user_database.user_database import *
 
 # temp imports, delete later #TODO
 CALIBRI_FONT = "Calibri"
-SEGOE_UI_FONT = "Segoe UI"
 
 
-class PyFloraPosude:
-    def __init__(self, root):
-        self.root = root
-        self.create_login_screen()
+def gui():
+    class PyFloraPosude:
+        def __init__(self, root):
+            self.root = root
+            self.create_login_screen()
 
-    # def login(self):
-    #     self.user = db_login(self.username.get(), self.password.get())
-    #     if self.user == None:
-    #         print("Na postoji korisnik s tim korisnickim imenom i lozinkom.")
-    #         return
-    #     print(f"Dobro dosli {self.user.name}")
-    #     self.clear_root()
-    #     self.create_main_screen()
+        def login(self):
+            self.user = db_login(self.username.get(), self.password.get())
+            if self.user == None:
+                print("Na postoji korisnik s tim korisnickim imenom i lozinkom.")
+                return
+            print(f"Dobro dosli {self.user.name}")
+            # self.clear_root()
+            # self.create_main_screen()
 
-    # def logout(self):
-    #     self.clear_root()
-    #     self.create_login_screen()
+        def logout(self):
+            self.clear_root()
+            self.create_login_screen()
 
-    def create_login_screen(self):
-        self.frame_login = tk.Frame(
-            self.root, highlightbackground="black", highlightthickness=1
-        )
-        self.frame_login.pack(fill=tk.BOTH, padx=10, pady=10)
+        def create_login_screen(self):
+            self.frame_login = tk.Frame(
+                self.root, highlightbackground="black", highlightthickness=1
+            )
+            self.frame_login.pack(fill=tk.BOTH, padx=10, pady=10)
 
-        lbl_header = tk.Label(
-            self.frame_login, text="PyFloraPosude", font=(SEGOE_UI_FONT, 14)
-        )
-        lbl_header.pack()
+            lbl_header = tk.Label(
+                self.frame_login, text="PyFloraPosude", font=(CALIBRI_FONT, 14)
+            )
+            lbl_header.pack()
 
-        lbl_login = tk.Label(self.frame_login, text="Prijava", font=(SEGOE_UI_FONT, 20))
-        lbl_login.pack(pady=20)
+            lbl_login = tk.Label(
+                self.frame_login, text="Prijava", font=(CALIBRI_FONT, 20)
+            )
+            lbl_login.pack(pady=20)
 
-        lbl_username = tk.Label(
-            self.frame_login,
-            text="Korisnicko ime",
-            font=(SEGOE_UI_FONT, 12),
-        )
-        lbl_username.pack()
+            lbl_username = tk.Label(
+                self.frame_login,
+                text="Korisnicko ime",
+                font=(CALIBRI_FONT, 12),
+            )
+            lbl_username.pack()
 
-        self.username = tk.StringVar()
-        ent_username = tk.Entry(self.frame_login, textvariable=self.username)
-        ent_username.pack(pady=5)
+            self.username = tk.StringVar()
+            ent_username = tk.Entry(self.frame_login, textvariable=self.username)
+            ent_username.pack(pady=5)
 
-        lbl_password = tk.Label(
-            self.frame_login, text="Lozinka", font=(SEGOE_UI_FONT, 12)
-        )
-        lbl_password.pack()
+            lbl_password = tk.Label(
+                self.frame_login, text="Lozinka", font=(CALIBRI_FONT, 12)
+            )
+            lbl_password.pack()
 
-        self.password = tk.StringVar()
-        ent_password = tk.Entry(self.frame_login, textvariable=self.password, show="*")
-        ent_password.pack(pady=5)
+            self.password = tk.StringVar()
+            ent_password = tk.Entry(
+                self.frame_login, textvariable=self.password, show="*"
+            )
+            ent_password.pack(pady=5)
 
-        btn_login = tk.Button(self.frame_login, text="Prijavi me")
-        btn_login.pack()
+            btn_login = tk.Button(
+                self.frame_login, text="Prijavi me", command=self.login
+            )
+            btn_login.pack()
 
-
-root = tk.Tk()
-root.geometry("700x500")
-root.title("Py Flora Posude")
-py_flora_posude = PyFloraPosude(root)
-root.mainloop()
+    root = tk.Tk()
+    root.geometry("700x500")
+    root.title("Py Flora Posude")
+    py_flora_posude = PyFloraPosude(root)
+    root.mainloop()
---
+++ user_database/user_database.py
@@ -0,0 +1,84 @@
+import sqlalchemy as db
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    surname = db.Column(db.String)
+    username = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+
+
+db_engine = db.create_engine("sqlite:///user_database/user_database.db")
+Base.metadata.create_all(bind=db_engine)
+
+
+# CRUD User
+def db_add_user(name, surname, username, password):
+    with Session(bind=db_engine) as session:
+        user_exists = (
+            session.query(User).filter(User.username == username).one_or_none()
+        )
+
+        if user_exists:
+            return
+
+        user = User(name=name, surname=surname, username=username, password=password)
+        session.add(user)
+        session.commit()
+
+
+def db_get_users():
+    with Session(bind=db_engine) as session:
+        users = session.query(User).all()
+        return users
+
+
+def db_update_user(user):
+    with Session(bind=db_engine) as session:
+        current_user = session.query(User).filter(User.id == user.id)
+        current_user.update(
+            values={
+                "name": user.name,
+                "surname": user.surname,
+                "username": user.username,
+                "password": user.password,
+            }
+        )
+        session.commit()
+
+
+def db_delete_user(user_id):
+    with Session(bind=db_engine) as session:
+        user = session.query(User).filter(User.id == user.id).one_or_none()
+
+        if user:
+            session.delete(user)
+            session.commit()
+        else:
+            pass  # TODO write message in gui that user doesn't exist
+
+
+# Login
+def db_login(username, password):
+    with Session(db_engine) as session:
+        user = (
+            session.query(User)
+            .filter(User.username == username, User.password == password)
+            .one_or_none()
+        )
+
+        return user
+
+
+db_add_user(
+    name="Daniel",
+    surname="Zima",
+    username="admin",
+    password="2222",
+)
